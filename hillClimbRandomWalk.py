c33cd4f605ecfd7edbaa4f1c6a0130568a96b38c
Jasmin Wilson <jasmin.wilson2005@gmail.com>
2022-04-26 21:19:46 -0500

    Add files via upload

---
+++ hillClimbRandomWalk.py
@@ -0,0 +1,184 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 11 22:48:06 2022
+
+@author: jasmi
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 10 22:49:48 2022
+
+@author: jasmi
+"""
+
+
+#basic hill climbing search provided as base code for the DSA/ISE 5113 course
+#author: Charles Nicholson
+#date revised: 3/26/2021
+
+#NOTE: YOU MAY CHANGE ALMOST ANYTHING YOU LIKE IN THIS CODE.  
+#However, I would like all students to have the same problem instance, therefore please do not change anything relating to:
+#   random number generator
+#   number of items (should be 150)
+#   random problem instance
+#   weight limit of the knapsack
+
+#------------------------------------------------------------------------------
+
+#Student name:Jasmin Wilson
+#Date: 04.10.2022
+
+
+#need some python libraries
+from random import Random   #need this for the random number generation -- do not change
+import numpy as np
+
+
+#to setup a random number generator, we will specify a "seed" value
+#need this for the random number generation -- do not change
+seed = 51132021
+myPRNG = Random(seed)
+
+#to get a random number between 0 and 1, use this:             myPRNG.random()
+#to get a random number between lwrBnd and upprBnd, use this:  myPRNG.uniform(lwrBnd,upprBnd)
+#to get a random integer between lwrBnd and upprBnd, use this: myPRNG.randint(lwrBnd,upprBnd)
+
+#number of elements in a solution
+n = 150
+
+#create an "instance" for the knapsack problem
+value = []
+for i in range(0,n):
+    value.append(round(myPRNG.triangular(150,2000,500),1))
+    
+weights = []
+for i in range(0,n):
+    weights.append(round(myPRNG.triangular(8,300,95),1))
+    
+#define max weight for the knapsack
+maxWeight = 2500
+
+k_restarts = 10    #number of restarts to do
+
+#change anything you like below this line ------------------------------------
+#some of the provided functions are intetionally incomplete
+#also, you may wish to restructure the approach entirely -- this is NOT the world's best Python code
+
+
+#monitor the number of solutions evaluated
+solutionsChecked = 0
+
+#function to evaluate a solution x
+def evaluate(x):
+          
+    a=np.array(x)
+    b=np.array(value)
+    c=np.array(weights)
+    
+    totalValue = np.dot(a,b)     #compute the value of the knapsack selection
+    totalWeight = np.dot(a,c)    #compute the weight value of the knapsack selection
+    
+    if totalWeight > maxWeight:
+        totalValue = -1
+    
+    return [totalValue, totalWeight]   #returns a list of both total value and total weight
+          
+       
+#here is a simple function to create a neighborhood
+#1-flip neighborhood of solution x         
+def neighborhood(x):
+        
+    nbrhood = []     
+    
+    for i in range(0,n):
+        nbrhood.append(np.copy(x))
+        if nbrhood[i][i] == 1:
+            nbrhood[i][i] = 0
+        else:
+            nbrhood[i][i] = 1
+      
+    return nbrhood
+          
+
+
+#create the initial solution
+def initial_solution():
+    x = []   #i recommend creating the solution as a list
+    weightCount=0   #weight counter
+    totalOnes=0     #total one's needed
+    #this loop will make sure of weight constraint of intial solution
+    while weightCount <= maxWeight:
+        weightCount= weightCount+ myPRNG.choice(weights) #adding randomness in choosing different weight than highest
+        totalOnes+=1 #keep a count until random weights equals 2500 or less
+        
+    x= np.zeros(n, dtype=int)   #fill x with 0's
+    x[np.random.randint(n, size=totalOnes)] = 1 #adding randomness is placing one's differently
+    total_weight = evaluate(x)[1]   #use evaluate function to get the total weight
+    
+    #makes sure of no infeasability
+    if total_weight > maxWeight: #if feasibility found, generate random again until totalWeight < maxWeight
+        x= np.zeros(n, dtype=int)   #fill x with 0's
+        while weightCount <= maxWeight:
+            weightCount= weightCount+ myPRNG.choice(weights)
+            totalOnes+=1
+            x[np.random.randint(n, size=totalOnes)] = 1 #randomly assing 1's
+            total_weight = evaluate(x)[1] #calculate totalWeight again
+    
+    return x
+
+#------ USING BEST-ACCEPT METHOD
+
+
+#varaible to record the number of solutions evaluated
+solutionsChecked = 0
+
+#store all the past solutions from the restarts 
+best_value = []     #stores the best values from all restarts
+probCheck =0.45    #with probability p
+random_walk =0      #flag to know if random walk is initiated 
+
+
+x_curr = initial_solution()  #x_curr will hold the current solution 
+x_best = x_curr[:]           #x_best will hold the best solution 
+f_curr = evaluate(x_curr)    #f_curr will hold the evaluation of the current soluton 
+f_best = f_curr[:]
+    
+    
+    
+#begin local search overall logic ----------------
+done = 0
+while done == 0:
+    Neighborhood = neighborhood(x_curr)  #create a list of all neighbors in the neighborhood of x_curr
+    probRand = myPRNG.random()
+    random_walk =0      #flag to know if random walk is initiated 
+    if probRand > probCheck:    #with probability 1-p, it will perform a random walk
+        while random_walk == 0:
+            solutionsChecked += 1
+            x_curr = myPRNG.choice(Neighborhood)    #choose random neighbor
+            if evaluate(x_curr)[0] >= 0:            #evaluate the best soltuion
+                x_best = x_curr[:]
+                f_curr = evaluate(x_curr)
+                f_best = f_curr
+                random_walk = 1
+                best_value.append(f_best)
+    else:                                   #with probability P, it will perform normal hill climbing
+        for s in Neighborhood:
+            solutionsChecked += 1
+            if evaluate(s)[0] > f_best[0]:
+                x_best = s[:]
+                f_best = evaluate(s)[:]
+                best_value.append(f_best)
+        if solutionsChecked >= 50000:          #stopping criterion
+            done = 1
+        else:
+            x_curr = x_best[:]
+            f_curr = f_best[:]
+    print("\nTotal number of solutions checked: ", solutionsChecked)
+    print("Best value found so far: ", f_best)
+    
+print ("\nFinal number of solutions checked: ", solutionsChecked)
+print ("Best value found: ", f_best[0])
+print ("Weight is: ", f_best[1])
+print ("Total number of items selected: ", np.sum(x_best))
+print ("Best solution: ", x_best)
